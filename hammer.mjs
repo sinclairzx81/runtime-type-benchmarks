dd6c5ddc613de508c5fc0712f0972a79bf571346
sinclair <haydn.developer@gmail.com>
2022-12-22 05:26:09 +0900

    re-run benchmarks post merge

--- hammer.mjs
+++ hammer.mjs
@@ -39,13 +39,14 @@ export async function format() {
 // -----------------------------------------------------------------------------
 // Benchmark
 // -----------------------------------------------------------------------------
-export async function measure(packageName = 'typebox_aot', compiler = 'tsc', dataset = 'correct', iteration = 10_000_000) {
+const measurement_iteration = 10_000_000
+export async function measure(packageName = 'typebox_aot', compiler = 'tsc', dataset = 'correct', iteration = measurement_iteration) {
   console.log(`\x1b[32mpackage: ${packageName} dataset: ${dataset} iteration: ${iteration}\x1b[0m`)
   const report_file = `reporting/results/${packageName}/${dataset}.json`
   await shell(`npx ${compiler} benchmark/validators/${packageName}/${dataset}.ts --outDir target/benchmark/${packageName}/${dataset}`)
   await shell(`node target/benchmark/${packageName}/${dataset}/validators/${packageName}/${dataset}.js ${iteration} ${report_file}`)
 }
-export async function typebox_aot(iteration = 10_000_000) {
+export async function typebox_aot(iteration = measurement_iteration) {
   TypeBoxAotGenerator.Build('benchmark/validators/typebox_aot', {
     allowArrayObjects: true,
     allowNaN: true,
@@ -53,7 +54,7 @@ export async function typebox_aot(iteration = 10_000_000) {
   await measure('typebox_aot', 'tsc', 'correct', iteration)
   await measure('typebox_aot', 'tsc', 'incorrect', iteration)
 }
-export async function typebox_jit(iteration = 10_000_000) {
+export async function typebox_jit(iteration = measurement_iteration) {
   TypeBoxJitGenerator.Build('benchmark/validators/typebox_jit', {
     allowArrayObjects: true,
     allowNaN: true,
@@ -61,27 +62,27 @@ export async function typebox_jit(iteration = 10_000_000) {
   await measure('typebox_jit', 'tsc', 'correct', iteration)
   await measure('typebox_jit', 'tsc', 'incorrect', iteration)
 }
-export async function ajv_aot(iteration = 10_000_000) {
+export async function ajv_aot(iteration = measurement_iteration) {
   AjvAotGenerator.Build('benchmark/validators/ajv_aot')
   await measure('ajv_aot', 'tsc', 'correct', iteration)
   await measure('ajv_aot', 'tsc', 'incorrect', iteration)
 }
-export async function ajv_jit(iteration = 10_000_000) {
+export async function ajv_jit(iteration = measurement_iteration) {
   AjvJitGenerator.Build('benchmark/validators/ajv_jit')
   await measure('ajv_jit', 'tsc', 'correct', iteration)
   await measure('ajv_jit', 'tsc', 'incorrect', iteration)
 }
-export async function tsrc(iteration = 10_000_000) {
+export async function tsrc(iteration = measurement_iteration) {
   TsrcGenerator.Build('benchmark/validators/tsrc')
   await measure('tsrc', 'ttsc', 'correct', iteration)
   await measure('tsrc', 'ttsc', 'incorrect', iteration)
 }
-export async function typia(iteration = 10_000_000) {
+export async function typia(iteration = measurement_iteration) {
   TypiaGenerator.Build('benchmark/validators/typia')
   await measure('typia', 'ttsc', 'correct', iteration)
   await measure('typia', 'ttsc', 'incorrect', iteration)
 }
-export async function benchmark(iteration = 10_000_000) {
+export async function benchmark(iteration = measurement_iteration) {
   await typebox_aot(iteration)
   await typebox_jit(iteration)
   await ajv_aot(iteration)
